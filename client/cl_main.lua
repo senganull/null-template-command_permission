922152c7540cbf249b17698c58745a7eea495a23
SengaNull <senga.null@gmail.com>
2026-02-07 23:58:09 +0900

    クライアント側処理を実装

---
+++ client/cl_main.lua
@@ -0,0 +1,14 @@
+RegisterNetEvent('null-template-command_permission:client:showNotification')
+AddEventHandler('null-template-command_permission:showNotification', function(message)
+    BeginTextCommandThefeedPost("STRING")
+    AddTextComponentSubstringPlayerName(message)
+    EndTextCommandThefeedPostTicker(false, false)
+end)
+
+--　権限不要なコマンド
+RegisterCommand('ping', function()
+    TriggerEvent('chat:addMessage', {
+        color = {0, 255, 255},
+        args = {"System", "Pong!"}
+    })
+end,false)
